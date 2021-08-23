d934ad97cba8b20e3d60c53837e6f52071516806
Ekaterina <tsurtsumiaekaterina@yandex.ru>
2021-08-23 22:03:11 +0300

    удаление лишних импортов

--- src/main/java/com/example/aircompany/api/CountryApi.java
+++ src/main/java/com/example/aircompany/api/CountryApi.java
@@ -1,18 +1,11 @@
 package com.example.aircompany.api;
 
-import com.example.aircompany.repository.CountryRepository;
-import com.example.aircompany.repository.PassengerRepository;
 import com.example.aircompany.service.CountryService;
 import com.example.aircompany.structure.Country;
-import com.example.aircompany.structure.Passenger;
-import com.example.aircompany.structure.Pilot;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
-
-import javax.persistence.EntityNotFoundException;
-import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/countries")
