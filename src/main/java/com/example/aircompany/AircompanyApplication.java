545a930407781e35646cce8f57f91c65c6e8db3e
Ekaterina <tsurtsumiaekaterina@yandex.ru>
2021-08-23 21:57:57 +0300

    AIR-12 удаление лишних аннотаций и импортов

--- src/main/java/com/example/aircompany/AircompanyApplication.java
+++ src/main/java/com/example/aircompany/AircompanyApplication.java
@@ -1,12 +1,8 @@
 package com.example.aircompany;
 
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.math.BigDecimal;
-
-@Slf4j
 @SpringBootApplication
 public class AircompanyApplication {
 
